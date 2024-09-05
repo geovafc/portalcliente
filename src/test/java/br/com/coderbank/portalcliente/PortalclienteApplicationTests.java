9373bb188036a1378fd618f6a6acf34db95643cc
geovanefreitas <geovane.freitasc@gmail.com>
2024-09-05 15:00:03 -0300

    init commit

---
+++ src/test/java/br/com/coderbank/portalcliente/PortalclienteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.coderbank.portalcliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalclienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
